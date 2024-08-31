377b3f2d203351f04e3e0ca21951e16baf4167ee
Amiran Dugiev <dugiev123@gmail.com>
2024-08-31 16:04:14 +0200

    trying to add some API

--- src/app.py
+++ src/app.py
@@ -4,7 +4,6 @@ from flask import Flask, request, render_template
 'from Databases.Weather import Weather, db'
 
 app = Flask(__name__)
-BASE_URL = 'https://fakestoreapi.com'
 
 @app.route("/")
 def main():
@@ -13,6 +12,6 @@ def main():
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    response = request.args.get(f"{BASE_URL}/products")
+    response = request.args.get('https://fakestoreapi.com/products')
     print(response.json())
     return "You entered: " + input_text
