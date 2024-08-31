ac17cae8226b3b6435f2c21b0ad1985c04a95890
Amiran Dugiev <dugiev123@gmail.com>
2024-08-31 15:50:16 +0200

    trying to add some API

--- src/app.py
+++ src/app.py
@@ -13,5 +13,5 @@ def main():
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    response = request.get(f"{BASE_URL}/products")
+    response = request.args.get(f"{BASE_URL}/products")
     return "You entered: " + input_text + response.json()
