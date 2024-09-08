7b91a906a5257021cc837e843401c8d6beae894c
Amiran Dugiev <dugiev123@gmail.com>
2024-09-08 12:42:10 +0200

    fixed up css

--- src/app.py
+++ src/app.py
@@ -15,15 +15,10 @@ def echo_input():
     search_input = request.form.get("user_input", "")
     populate_db()
     queries = return_store(search_input)
-    count_avg = round(return_count_average(queries), 2)
-    price_avg = round(return_price_average(queries), 2)
-    rating_avg = round(return_rating_average(queries), 2)
-    return render_template("output.html",
-                           count_avg=count_avg,
-                           price_avg=price_avg,
-                           rating_avg=rating_avg,
-                           queries=queries,
-                           search_input=search_input)
+    count_avg = return_count_average(queries)
+    price_avg = return_price_average(queries)
+    rating_avg = return_rating_average(queries)
+    return render_template("output.html", count_avg=count_avg, price_avg=price_avg, rating_avg=rating_avg, queries=queries)
 
 if __name__ == "__main__":
     app.run(debug=True)
